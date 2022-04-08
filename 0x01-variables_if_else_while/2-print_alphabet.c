481a96a53c5be67e633a61aa2a611ba4be56b006
T0KICH0 <nickhailut@gmail.com>
2022-04-08 15:39:57 -0700

    added 2nd script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a', ch = 'z', ch++)
+{
+putchar(ch)
+}
+return (0)
+}
