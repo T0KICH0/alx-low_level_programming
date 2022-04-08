5fa01f83da19da72b7392af0551a9fd13c2d7f18
T0KICH0 <nickhailut@gmail.com>
2022-04-08 16:10:55 -0700

    edit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,12 @@
 int main(void)
 {
 char ch = 'a';
-for (ch = 'a'; ch <= 'z' && ch != 'q' && ch != 'e' ; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if ((ch != 'e') && (ch != 'q'))
 {
 putchar(ch);
+}
 }
 putchar('\n');
 return (0);
