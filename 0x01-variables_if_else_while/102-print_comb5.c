bf752ecda445bb619e5d77a48090a72be232e765
T0KICH0 <nickhailut@gmail.com>
2022-05-26 12:58:04 +0300

    added 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main - Prints combination of numbers
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+int c, i, k, o;
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+for (o = '0'; o <= '9'; o++)
+{
+if (i < o && c==k)
+{
+putchar(c);
+putchar(i);
+putchar(' ');
+putchar(k);
+putchar(o);
+if (!(c=='9' && i=='8' && k=='9' && o=='9' ))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
