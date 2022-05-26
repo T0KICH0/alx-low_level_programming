e0f660650c75a6c5af0e14fe4236dc55caf6ce31
T0KICH0 <nickhailut@gmail.com>
2022-05-26 13:13:19 +0300

    edit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
+
 /**
-* main - Prints combination of numbers
-*
-* Return: Always (Success)
-*/
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-int c, i, k, o;
-for (c = '0'; c <= '9'; c++)
-{
-for (i = '0'; i <= '9'; i++)
-{
-for (k = '0'; k <= '9'; k++)
-{
-for (o = '0'; o <= '9'; o++)
-{
-if (i < o)
-{
-putchar(c);
-putchar(i);
-putchar(' ');
-putchar(k);
-putchar(o);
-if (!(c == '9' && i == '8' && k == '9' && o == '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
-}
+	int c, i, k, j;
 
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
