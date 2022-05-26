34123d41feb9b000d28802cc1e29e9f8f5da0da4
T0KICH0 <nickhailut@gmail.com>
2022-05-26 18:40:59 +0300

    edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,103 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *prints times table based on n
- *
- *The int to print
- *Return: Always 0.
-*/
+  * print_times_table - Prints a multiplication table up to param
+  * @n: The number to be treated
+  *
+  * Return: Number matrix
+  */
 void print_times_table(int n)
 {
-if (n > 0 && n <= 15)
-{
-int k, j, l;
-for (k = 0; k <= n; k++)
-{
-for (j = 0; j <= n; j++)
-{
-if (j == 0)
-{
-l = k * j;
-if (l < 10)
-{
-putchar(l + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
-else if(l >= 10 && l <= 99)
-{
-putchar(l / 10 + '0');
-putchar(l % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
-}
-else if(l > 99)
-{
-putchar('\b');
-putchar(l / 100 + '0');
-putchar((l - 100) / 10 + '0');
-putchar(l % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
-}
-}
-else if (j < n && j > 0)
-{
-l = k * j;
-if (l < 10)
-{
-putchar(' ');
-putchar(l + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
-}
-else if(l >= 10 && l <= 99)
-{
-putchar(l / 10 + '0');
-putchar(l % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
-}
-else if(l > 99)
-{
-putchar('\b');
-putchar(l / 100 + '0');
-putchar((l - 100) / 10 + '0');
-putchar(l % 10 + '0');
-putchar(',');
-putchar(' ');
-putchar(' ');
-}
-}
-else if (j == n)
-{
-l = k *j;
-if (l < 10)
-{
-putchar(' ');
-putchar(l + '0');
-}
-else if(l >= 10 && l <= 99)
-{
-putchar(l / 10 + '0');
-putchar(l % 10 + '0');
-}
-else if(l>99)
-{
-putchar('\b');
-putchar(l / 100 + '0');
-putchar((l - 100) / 10 + '0');
-putchar(l % 10 + '0');
-}
-}
-}
-putchar('\n');
-}
-}
-}
+
